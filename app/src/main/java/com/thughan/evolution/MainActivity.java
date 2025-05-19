8fd4813064bfe00f5392f38825ce9f2abd4652c1
thug-han <hjl999@126.com>
2025-05-19 17:21:06 +0800

    添加Compose界面 增加各module变量逻辑在lib和application变换

--- app/src/main/java/com/thughan/evolution/MainActivity.java
+++ app/src/main/java/com/thughan/evolution/MainActivity.java
@@ -9,21 +9,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.thughan.android.ModuleConstants;
 import com.thughan.designmode.DesignConstants;
+import com.thughan.kotlin.KotlinConstants;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button mDesignMode;
     private Button mLeakCanary;
+    private Button mCompose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ARouter.init(getApplication());
+        ARouter.openDebug();
+        ARouter.openLog();
+
         mDesignMode = findViewById(R.id.btn_design_mode);
         mLeakCanary = findViewById(R.id.btn_leak_canary);
+        mCompose = findViewById(R.id.btn_compose);
 
         mLeakCanary.setOnClickListener(this);
         mDesignMode.setOnClickListener(this);
+        mCompose.setOnClickListener(this);
     }
 
     @Override
@@ -33,6 +40,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             ARouter.getInstance().build(DesignConstants.ACTIVITY_PATH).navigation();
         } else if (id == R.id.btn_leak_canary) {
             ARouter.getInstance().build(ModuleConstants.ACTIVITY_PATH).navigation();
+        } else if (id == R.id.btn_compose) {
+            ARouter.getInstance().build(KotlinConstants.ACTIVITY_COMPOSE_PATH).navigation();
         }
     }
 }
